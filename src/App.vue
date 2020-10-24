03c8be6e514629141de8c8059178881c7e7cf1a6
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-10-24 10:49:29 -0300

    #main it fixs the feature of table responsive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app" class="container">
-    <div class="row">
-      <div class="col-12">
-        <SwPlanets />
-      </div>
-    </div>
+    <SwPlanets />
   </div>
 </template>
 
