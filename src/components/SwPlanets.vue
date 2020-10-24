18966381556b715de47cd431fe0354862f1149a4
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-10-24 07:28:52 -0300

    #main first version of show film info

--- src/components/SwPlanets.vue
+++ src/components/SwPlanets.vue
@@ -8,47 +8,68 @@
       striped
       outlined
       responsive
-    />
+    >
+      <template #cell(films)="row">
+        <b-button
+          v-b-modal.modal-1
+          @click="getFilmsWherePlanetAppear(row.item.films)"
+          >Films</b-button
+        >
+      </template>
+    </b-table>
+    <b-modal id="modal-1" title="BootstrapVue">
+      <div v-for="(film, index) in filmsTitles" :key="index">
+        {{ film }}
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  data () {
+  data() {
     return {
+      filmsTitles: [],
       fields: [
-        { key: 'name', sortable: true },
-        { key: 'climate', sortable: true },
-        { key: 'diameter', sortable: true },
-        { key: 'films', sortable: true },
-        { key: 'gravity', sortable: true },
-        { key: 'orbital_period', sortable: true },
-        { key: 'population', sortable: true },
-        { key: 'residents', sortable: true },
-        { key: 'rotation_period', sortable: true },
-        { key: 'surface_water', sortable: true },
-        { key: 'terrain', sortable: true },
-        { key: 'created', sortable: true },
-        { key: 'edited', sortable: true },
-        { key: 'url', sortable: true },
+        { key: "name", sortable: true },
+        { key: "climate", sortable: true },
+        { key: "diameter", sortable: true },
+        { key: "films", sortable: true },
+        { key: "gravity", sortable: true },
+        { key: "orbital_period", sortable: true },
+        { key: "population", sortable: true },
+        { key: "residents", sortable: true },
+        { key: "rotation_period", sortable: true },
+        { key: "surface_water", sortable: true },
+        { key: "terrain", sortable: true },
+        { key: "created", sortable: true },
+        { key: "edited", sortable: true },
+        { key: "url", sortable: true }
       ],
       planets: []
+    };
+  },
+  methods: {
+    async getPlanets() {
+      const apiResponse = await axios.get("https://swapi.dev/api/planets/");
+      this.planets = apiResponse.data.results;
+    },
+    async getFilmsWherePlanetAppear(arrayUrl) {
+      const arrayOfPromises = arrayUrl.map(async url => {
+        const apiResponse = await axios.get(url);
+        return apiResponse.data.title;
+      });
+      (async () => {
+        const filmsTitles = await Promise.all(arrayOfPromises);
+        this.filmsTitles = filmsTitles;
+      })();
     }
   },
-  methods:{
-    async getPlanets(){
-      console.log("getPlanets");
-      const apiResponse = await axios.get("https://swapi.dev/api/planets/")
-      this.planets = apiResponse.data.results
-    }
-  },
-  created(){
-    this.getPlanets()
+  created() {
+    this.getPlanets();
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
